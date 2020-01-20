0d732582cbf16786cf9e217af3f0a81935a5ef3d
Masaho Tamura <tamuramasaho753@gmail.com>
2020-01-20 09:24:57 +0900

    add everything

--- dist/my-page.e31bb0bc.js
+++ dist/my-page.e31bb0bc.js
@@ -118,7 +118,18 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
-
+$(function () {
+  $('a[href^="#"]').click(function () {
+    var speed = 500;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+});
 },{}],"../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +158,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63303" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53066" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
